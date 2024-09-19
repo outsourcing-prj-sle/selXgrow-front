2af236af4fd6a1d5c6f9d1bac18faad104daa472
chw970320 <chw970320@gmail.com>
2024-09-20 01:09:53 +0900

    feat: commonButton 생성

---
+++ src/components/ButtonItems.vue
@@ -0,0 +1,32 @@
+<template>
+    <button className="bg-[#82c2fd] px-6 py-2 font-bold text-2xl rounded-full" type="button">{{ selectedItem.content }}</button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+    const props = defineProps({
+        name: {
+            type: String,
+            required: true
+        }
+    })
+
+    const meta = [
+        {
+            name: "prev",
+            content: "←"
+        },
+        {
+            name: "next",
+            content: "→"
+        }
+    ]
+
+    const selectedItem = computed(() => {
+        return meta.find((m) => m.name === props.name)
+    });
+</script>
+
+<style scoped>
+</style>
