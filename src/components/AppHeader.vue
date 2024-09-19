6e64fad6c681a84f720b49ea5b580bc373706ad1
hwansoo <realyhs1@gmail.com>
2024-09-19 20:42:47 +0900

    헤더수정

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,12 @@
       v-if="showHeader"
     >
       <div class="font-bold text-3xl text-black">
-        01.Mindset Survey
+        {{ title }}
       </div>
       <div class="flex">
         <div class="text-base font-bold text-blue-300 mr-4 content-end">
           09.20.2024 ~ 09.28.2024
+          <!-- {{ date }} -->
         </div>
         <div class="cursor-pointer" @click="goMyInfo">
           <img
@@ -45,6 +46,9 @@
   const userStore = useUserStore();
   const userId = computed(() => userStore.token);
   const loginType = computed(() => userStore.type);
+  // todo :: route.meta.title || { stateStore.title } || 'Title'
+  const title = computed(() => route.meta.headerTitle || 'Title');
+  // todo :: const date = computed(() => route.meta.needDate && { stateStore.title });
 
   const logout = () => {
     alert('로그아웃')
