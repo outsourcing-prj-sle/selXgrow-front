df80db026273afdc11abda574b14f5987d68c828
chw970320 <chw970320@gmail.com>
2024-09-20 01:55:54 +0900

    feat: diary2 화면 작업 완료

---
+++ src/components/MoodItem.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+        <picture className="bg-[#f3f5f7] rounded-full flex items-center justify-center">
+            <img
+            :className="`${isLowNumber && 'p-4'}`"
+            :src="require(`@/assets/img/${selectedMood.icon}.png`)"
+            alt="mood icon"
+            />
+        </picture>
+        <p v-if="showText && !isLowNumber">Extremely</p>
+        <p v-if="showText && isLowNumber">A Litter</p>
+    </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    showText: {
+        type: Boolean,
+        default: false
+    },
+    isLowNumber: {
+        type: Boolean,
+        default: false
+    },
+})
+
+const moods = [
+    { 
+        name: 'Sad', 
+        icon: 'diary1-sad-icon', 
+        color: '#007CD6' 
+    },
+    {
+        name: 'Angry',
+        icon: 'diary1-angry-icon',
+        color: '#B81521',
+    },
+    {
+        name: 'Excited',
+        icon: 'diary1-excited-icon',
+        color: '#F96D30',
+    },
+    {
+        name: 'Content',
+        icon: 'diary1-content-icon',
+        color: '#D9B341',
+    },
+    {
+        name: 'Thankful',
+        icon: 'diary1-thankful-icon',
+        color: '#BFBB30',
+    },
+    {
+        name: 'Proud',
+        icon: 'diary1-proud-icon',
+        color: '#A0BF00',
+    },
+    {
+        name: 'Happy',
+        icon: 'diary1-happy-icon',
+        color: '#CFAE08',
+    },
+    {
+        name: 'Confused',
+        icon: 'diary1-confused-icon',
+        color: '#0F9B35',
+    },
+    {
+        name: 'Bored',
+        icon: 'diary1-bored-icon',
+        color: '#812088',
+    },
+    {
+        name: 'Embarrassed',
+        icon: 'diary1-embarrassed-icon',
+        color: '#F1649B',
+    },
+    {
+        name: 'Worried',
+        icon: 'diary1-worried-icon',
+        color: '#57B109',
+    },
+];
+
+const selectedMood = computed(() => {
+    return moods.find((m) => m.name === props.name)
+});
+</script>
+
+<style scoped>
+    p {
+        font-weight: bold;
+        padding: 10px 0;
+        font-size: 18px;
+    }
+</style>
--- src/views/Diary2View.vue
+++ src/views/Diary2View.vue
@@ -1,29 +1,55 @@
 <template>
-    <div className="flex items-start">
-        <RobotItem />
-        <p className="font-bold px-4 py-2 mt-4 rounded-md bg-white border-2 border-[#F6F0FF] text-left">
-            Next, use the slider to show the 
-            <span className="underline underline-offset-2">intensity</span>
-            of your emotion, or 
-            <span className="underline underline-offset-2">how much</span>
-            you are feeling that way
-        </p>
-    </div>
-    <div className="flex flex-col px-36 justify-center items-center gap-12">
-        <p className="text-white w-full bg-[#494857] py-4 break-all text-2xl rounded-xl">
-            How <span className="text-[#80beeb]">{{ props.selectedFeel }}</span> do you feel?
-        </p>
-        <div className="flex flex-col w-28 aspect-square bg-[#19146a] rounded-2xl justify-center">
-            <p className="text-white text-4xl">{{ feelScore }}</p>
-            <p className="text-white opacity-50">{{ feelDescription }}</p>
+    <div className="flex flex-col justify-between h-full mb-8">
+        <div>
+            <div className="flex items-start">
+                <RobotItem />
+                <p className="font-bold px-4 py-2 mt-4 rounded-md bg-white border-2 border-[#F6F0FF] text-left">
+                    Next, use the slider to show the 
+                    <span className="underline underline-offset-2">intensity</span>
+                    of your emotion, or 
+                    <span className="underline underline-offset-2">how much</span>
+                    you are feeling that way
+                </p>
+            </div>
+            <div className="flex flex-col px-36 justify-center items-center">
+                <p className="text-white w-full bg-[#494857] py-4 break-all text-2xl rounded-xl">
+                    How <span className="text-[#80beeb]">{{ props.selectedFeel }}</span> do you feel?
+                </p>
+                <div className="flex flex-col w-28 aspect-square bg-[#19146a] rounded-2xl justify-center mt-12 -mb-6">
+                    <p className="text-white text-4xl">{{ feelScore }}</p>
+                    <p className="text-white opacity-50">{{ feelDescription }}</p>
+                </div>
+                <div className="flex justify-between items-center w-full px-16 gap-8">
+                    <MoodItem name="Sad" showText="true" isLowNumber="true" />
+                    <div className="bg-gradient-to-r from-white to-[#8785B2] rounded-2xl flex w-full justify-between items-center px-2 py-1">
+                        <button 
+                            v-for="i in [1, 2, 3, 4, 5]" 
+                            :key="i" 
+                            className="font-bold aspect-square px-2 rounded-full" 
+                            type="button"
+                            @click="clickFeelScoreHandler(i)"
+                        >
+                        {{ i }}
+                        </button>
+                    </div>
+                    <MoodItem name="Sad" showText="true" />
+                </div>
+            </div>
+        </div>
+        <div className="flex justify-between items-center">
+            <ButtonItems name="prev" @click="() => router.push({ name: 'diary1' })" />
+            <ButtonItems name="next" @click="() => router.push({ name: 'diary3' })"/>
         </div>
     </div>
 </template>
 
 <script setup>
-import RobotItem from '@/components/RobotItem.vue';
-
 import { computed, ref } from 'vue';
+import router from '@/router';
+
+import ButtonItems from '@/components/ButtonItems.vue';
+import MoodItem from '@/components/MoodItem.vue';
+import RobotItem from '@/components/RobotItem.vue';
 
 const props = defineProps({
     selectedFeel: {
@@ -32,11 +58,16 @@ const props = defineProps({
         default: 'sad'
     }
 });
-const feelScore = ref(1)
+
+const feelScore = ref(1);
 
 const feelDescription = computed(() => {
     return ['A Little', 'Pretty', 'Quite', 'Very', 'Extremely'][feelScore.value - 1]
-})
+});
+
+const clickFeelScoreHandler = (number) => {
+    feelScore.value = number;
+};
 </script>
 
 <style scoped>
