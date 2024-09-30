aacbfdc38595cc7d94a2404ba9e3e9e1e20ef278
hwansoo <realyhs1@gmail.com>
2024-09-30 20:41:56 +0900

    페이지 표기 다시 추가

--- src/views/CheckInReportView.vue
+++ src/views/CheckInReportView.vue
@@ -26,7 +26,7 @@
         </div>
         <div class="flex flex-1 flex-col">
           <!-- 진행도 막대 -->
-          <div class="w-full flex flex-col justify-end items-end mb-2">
+          <div class="w-full flex flex-col justify-end items-end">
             <div class="w-full justify-end flex h-1">
               <div
                 v-for="i in totalQuestion"
@@ -39,9 +39,9 @@
                 ></div>
               </div>
             </div>
-            <!-- <div class="text-[#A7A7A7] font-normal text-sm mt-1">
+            <div class="text-[#A7A7A7] font-normal text-sm mt-1">
               {{ `${nowPage} / ${totalQuestion}` }}
-            </div> -->
+            </div>
           </div>
           <template v-if="!showWatchCarefully">
             <!-- 로봇 멘트 -->
